db60b5ede18f1c806980e10037a61a92005e8e93
clofour <208706398+clofour@users.noreply.github.com>
2026-05-02 00:40:59 +0200

    Bug fixes

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -26,5 +26,16 @@ provider "flux" {
     }
     git = {
         url = var.git_repo
+        http = {}
+    }
+}
+
+provider "helm" {
+    kubernetes = {
+        host = digitalocean_kubernetes_cluster.main.endpoint
+        token = digitalocean_kubernetes_cluster.main.kube_config[0].token
+        cluster_ca_certificate = base64decode(
+            digitalocean_kubernetes_cluster.main.kube_config[0].cluster_ca_certificate
+        )
     }
 }
--- terraform/versions.tf
+++ terraform/versions.tf
@@ -22,5 +22,9 @@ terraform {
         source = "hashicorp/random"
         version = "~> 3.8.1"
       }
+      helm = {
+        source = "hashicorp/helm"
+        version = "~> 3.1.1"
+      }
     }
 }
