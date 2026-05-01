55202b134e8f36d56ba3179340e5e34c4e916d01
clofour <208706398+clofour@users.noreply.github.com>
2026-05-01 23:10:50 +0200

    Ditto

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -1,22 +1,3 @@
-locals {
-    lb_ip = try(data.kubernetes_service_v1.envoy_gateway.status[0].load_balancer[0].ingress[0].ip, null)
-    records = toset([
-        var.gitlab_host, var.registry_host, "*.${var.pages_host}", var.grafana_host
-    ])
-}
-
 resource "digitalocean_domain" "main" {
     name = var.domain_name
 }
-
-# resource "digitalocean_record" "main" {
-#     for_each = local.records
-
-#     domain = digitalocean_domain.main.name
-#     type = "A"
-#     name = each.key
-#     value = local.lb_ip
-#     ttl = 300
-
-#     depends_on = [ time_sleep.wait_for_gateway_debug ]
-# }
